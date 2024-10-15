b81b0b9d1d5f135379818add0a9895aba01ebcaf
mareye <111225095+mareye132@users.noreply.github.com>
2024-10-15 18:59:20 +0300

    Update database.py

--- Project/database.py
+++ Project/database.py
@@ -1,1 +1,20 @@
 
+# database.py
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+DATABASE_URL = "sqlite:///./test.db"  # Use your actual database URL
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# Dependency to get the DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
